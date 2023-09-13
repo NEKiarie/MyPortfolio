377e3ac0bd6e2b50b17f3df107cd7380ac8cdb6b
NEKiarie <s49188705@gmail.com>
2023-09-13 09:44:41 +0300

    :sparkles:Projects Section completed

--- app/projects/page.js
+++ app/projects/page.js
@@ -7,14 +7,17 @@ import AnimatedText from "../components/AnimatedText";
 import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "../components/Icons";
-import project1 from "../../public/images/Portfolio.png";
+import FeaturedProject1 from "../../public/images/Portfolio.png";
+import Project1 from "../../public/images/Steer X.png";
+import Project2 from "../../public/images/Airbnb.png";
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
     <article
       className="w-full flex items-center justify-between rounded-3xl
-    border border-solid border-dark2 bg-light shadow-2xl p-12 "
+    border border-solid border-dark2 bg-light shadow-2xl p-12 relative rounded-br-2xl"
     >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark2 rounded-br-3xl"  />
       <Link
         href={link}
         target="_blank"
@@ -52,6 +55,48 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   );
 };
 
+const Project = ({ title, type, img, link, github}) => {
+  return(
+   <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid
+   border-dark2 bg-light p-6 relative rounded-br-2xl ">
+    <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark2 rounded-br-3xl"  />
+    <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg
+      "
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl ">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+        <div className=" w-full mt-2 flex items-center justify-between">
+        <Link
+            href={link}
+            target="_blank"
+            className="text-lg font-semibold underline text-dark2"
+          >
+            Vist
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+
+        
+        </div>
+      </div>
+   </article>
+  )
+}
+
 function page() {
   return (
     <>
@@ -66,23 +111,34 @@ function page() {
             className="mb-16"
           />
 
-          <div className="grid grid-cols-12 gap-24 ">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="React Portfolio Website"
-                img={project1}
-                summary="A feature-rich Crypto Screener App using React, 
-                  Tailwind CSS, Context API, React Router and Recharts. 
-                  It shows detail regarding almost all the cryptocurrency. 
-                 You can easily convert the price in your local currency."
+                img={FeaturedProject1}
+                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
+                page transitions, cool background effects, unique design and it is mobile responsive.
+                ."
                 link="/"
                 github="/"
                 type="Featured Project"
               />
             </div>
 
-            <div className="col-span-6">Project-1</div>
-            <div className="col-span-6">Project-2</div>
+            <div className="col-span-6"> <Project
+                title="Steer X"
+                img={Project1}
+                link="/"
+                github="/"
+                type="Featured Project"
+              /></div>
+            <div className="col-span-6"><Project
+                title="Full-Stack Airbnb Clone"
+                img={Project2}
+                link="/"
+                github="/"
+                type="Featured Project"
+              /></div>
           </div>
         </Layout>
       </main>
