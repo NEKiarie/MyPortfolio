31d142dc1f2e85ee439463c098f24d9fcdd89423
NEKiarie <s49188705@gmail.com>
2023-09-14 22:55:00 +0300

    :sparkles:Added Biography & Skills sections responsiveness

--- app/about/page.js
+++ app/about/page.js
@@ -44,9 +44,13 @@ function about() {
       </Head>
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start">
+          <AnimatedText
+            text="Passion Fuels Purpose!"
+            className="mb-16
+             lg:text-7xl sm:6xl xs:text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mt-4 text-sm font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography
               </h2>
@@ -73,7 +77,7 @@ function about() {
 
             <div
               className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark2
-             bg-light p-8 dark:bg-dark dark:border-light"
+             bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8"
             >
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark2 dark:bg-light" />
               <Image
@@ -86,29 +90,34 @@ function about() {
               />
             </div>
 
-            <div className="col-span-2 flex flex-col items-end justify-between">
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
+            <div
+              className="col-span-2 flex flex-col items-end 
+            justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3"
+            >
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={5} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75">
+                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm">
                   Satisfied Clients
                 </h2>
               </div>
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={15} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75">
+                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm">
                   Projects Completed
                 </h2>
               </div>
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
-                  {" "}
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={2} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75">
+                <h2 className="text-xl font-medium capitalize text-dark2/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm">
                   Years of Experience
                 </h2>
               </div>
--- app/components/Skills.js
+++ app/components/Skills.js
@@ -5,7 +5,9 @@ const Skill = ({ name, x, y }) => {
   return (
     <motion.div
       className="flex items-center justify-center rounded-full font-semibold
-           bg-dark2 text-light py-3 px-6 shadow-dark2 cursor-pointer absolute"
+           bg-dark2 text-light py-3 px-6 shadow-dark2 cursor-pointer absolute 
+           lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 dark:text-dark dark:bg-light
+           xs:bg-transparent xs:dark:bg-transparent xs:text-light xs:dark:text-dark xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration:1.5} }}
@@ -19,11 +21,16 @@ const Skill = ({ name, x, y }) => {
 function Skills() {
   return (
     <>
-      <h2 className="font-bold text-6xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark">
+      <h2 className="font-bold text-6xl mt-64 w-full text-center md:text-6xl md:mt-32">Skills</h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight 
+      dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
+       lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+       md:bg-circularLightMd md:dark:bg-circularDarkMd
+       sm:bg-circularLightSm sm:dark:bg-circularDarkSm">
         <motion.div
           className="flex items-center justify-center rounded-full font-semibold
-           bg-dark2 text-light p-8 shadow-dark2 cursor-pointer"
+           bg-dark2 text-light p-8 shadow-dark2 cursor-pointer dark:text-dark dark:bg-light lg:p-6
+           md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
           Web
