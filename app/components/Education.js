6012cb80a40b32e6febe77a909e71ea0fb45793f
NEKiarie <s49188705@gmail.com>
2023-09-18 15:35:37 +0300

    :sparkles:Education section responsiveness complete

--- app/components/Education.js
+++ app/components/Education.js
@@ -9,7 +9,7 @@ const Details = ({ type, time, place, info }) => {
     <li
       ref={ref}
       className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col 
-    items-center justify-between"
+    items-center justify-between md:w-[80%]"
     >
       <LiIcon reference={ref} />
       <motion.div
@@ -17,11 +17,11 @@ const Details = ({ type, time, place, info }) => {
         whileInView={{ y: 0 }}
         transition={{ duration: 0.5, type: "spring" }}
       >
-        <h3 className="capitalize font-bold text-2xl text-dark2 dark:text-light/75">{type}</h3>
-        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
+        <h3 className="capitalize font-bold text-2xl text-dark2 dark:text-light/75 sm:text-xl xs:text-lg">{type}</h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {place}
         </span>
-        <p className="font-medium w-full">{info}</p>
+        <p className="font-medium w-full md:text-sm">{info}</p>
       </motion.div>
     </li>
   );
@@ -36,15 +36,16 @@ function Education() {
 
   return (
     <div className="my-64 ">
-      <h2 className="font-bold text-6xl mb-32 w-full text-center">Education</h2>
-      <div className="w-[75%] mx-auto relative ">
+      <h2 className="font-bold text-6xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">Education</h2>
+      <div className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
           ref={ref}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark2 origin-top dark:bg-light"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark2 origin-top dark:bg-light
+          md:w-[2px] md:left-[30px] xs:left-[20px]"
         />
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
             type={"Cisco Certified Support Technician(CCST)"}
             time={"2023"}
