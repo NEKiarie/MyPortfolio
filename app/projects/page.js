411091162e4efa2734d97b8a72c569a85257599b
NEKiarie <s49188705@gmail.com>
2023-09-25 11:26:53 +0300

    :sparkles:Added new project

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,10 +8,13 @@ import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "../components/Icons";
 import FeaturedProject1 from "../../public/images/Portfolio.png";
+import Project3 from "../../public/images/Messenger clone.jpg";
 import Project1 from "../../public/images/Steer X.png";
 import Project2 from "../../public/images/Airbnb.png";
 import { motion } from "framer-motion";
 import TransitionEffect from "../components/TransitionEffect";
+
+
 
 const FramerImage = motion(Image);
 
@@ -23,9 +26,11 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
      p-12 relative rounded-br-2xl dark:bg-dark dark:border-light lg:flex-col lg:p-8
      xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
     >
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] 
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] 
       rounded-[2rem] bg-dark2 rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%]
-      xs:w-full xs:rounded-[1rem]" />
+      xs:w-full xs:rounded-[1rem]"
+      />
       <Link
         href={link}
         target="_blank"
@@ -46,16 +51,24 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base
-        ">{type}</span>
+        <span
+          className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base
+        "
+        >
+          {type}
+        </span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">
+            {title}
+          </h2>
         </Link>
-        <p className="my-2 font-bold text-dark dark:text-light sm-text-sm">{summary}</p>
+        <p className="my-2 font-bold text-dark dark:text-light sm-text-sm">
+          {summary}
+        </p>
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10">
             <GithubIcon />
@@ -81,9 +94,11 @@ const Project = ({ title, type, img, link, github }) => {
       className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid
    border-dark2 bg-light p-6 relative rounded-br-2xl dark:bg-dark dark:border-light xs:p-4"
     >
-      <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem]
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem]
        bg-dark2 rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%]
-       xs:rounded-[1rem]" />
+       xs:rounded-[1rem]"
+      />
       <Link
         href={link}
         target="_blank"
@@ -100,15 +115,23 @@ const Project = ({ title, type, img, link, github }) => {
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg 
-        md:text-base">{type}</span>
+        <span
+          className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg 
+        md:text-base"
+        >
+          {type}
+        </span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl
-          ">{title}</h2>
+          <h2
+            className="my-2 w-full text-left text-3xl font-bold lg:text-2xl
+          "
+          >
+            {title}
+          </h2>
         </Link>
         <div className=" w-full mt-2 flex items-center justify-between">
           <Link
@@ -143,8 +166,10 @@ function page() {
             className="mb-16 lg:text-7xl sm:mb-8 sm:text-6xl xs:text-4xl"
           />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 
-          lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+          <div
+            className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 
+          lg:gap-x-8 md:gap-y-24 sm:gap-x-0"
+          >
             <div className="col-span-12 ">
               <FeaturedProject
                 title="React Portfolio Website"
@@ -176,6 +201,16 @@ function page() {
                 type="Featured Project"
               />
             </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                title="Real-Time Messenger Clone"
+                img={Project3}
+                link="https://real-time-messenger-clone-beta.vercel.app/"
+                github="https://github.com/NEKiarie/Real-Time-Messenger-Clone"
+                type="Featured Project"
+              />
+            </div>
+            
           </div>
         </Layout>
       </main>
